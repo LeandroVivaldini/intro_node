d26627add064c901ca0fec5ccb7346e24804cb6a
Leandro Vivaldini <LeandroVivaldini@users.noreply.github.com>
2024-02-20 22:50:15 -0300

    Aula 20/02/24

---
+++ aula_entrada.js
@@ -0,0 +1,9 @@
+let entrada = require('prompt-sync')();
+
+let nome;
+
+nome = entrada('Digite seu Nome: ');
+
+console.log(`O nome digitado foi: `${nome});
+
+entrada('Pressione ENTER para encerrar!');
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ let altura = 1.73;
 console.log('Nome: ' + nome + ' - Altura: ' + altura + ' - Idade: ' + idade + ' anos.');
 console.log(`Nome: ${nome} - Idade: ${idade} anos`);
 
+//Determinando o valor verdadeiro ou falso para a variável
 let trabalha = true;
 
 console.log(trabalha);
@@ -24,7 +25,7 @@ else{
 }
 
 const sangue = 'A+';
-
+// Apresenta os valores dos dados que foram armazenados nas variáveis nome, idade, altura, trabalho, e constante
 console.log(
     'vNome: ' + typeof(nome) + '\n' +
     'vIdade: ' + typeof(idade) + '\n' +
@@ -33,6 +34,7 @@ console.log(
     'vConstante: ' + typeof(sangue) + '\n' 
 );
 
+// Apresenta os dados armazenados na variável endereço
 let endereco = {
     'rua' : 'Brasil',
     'num' : '108',
@@ -45,12 +47,18 @@ let endereco = {
 console.log(endereco);
 console.log(typeof(endereco));
 
+//Varios dados armazenados dentro da variável frutas
 let frutas = ['banana' , 'mamão' , 'maçã'];
 console.log(frutas);
 
 let velha = [
 
-    [0, 1, 2]
-    [3, 4, 5]
+    [0, 1, 2],
+    [3, 4, 5],
     [6, 7, 8]
-]
+];
+
+//Apresenta os dados da matriz 
+console.log(velha[2][2]);
+console.log(typeof (velha));
+console.log((Array.isArray(velha)) ? 'Array': 'Não é Array');
