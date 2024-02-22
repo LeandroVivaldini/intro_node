50370ea63ba39b202d38f9f868c273e6a8df97c1
Leandro Vivaldini <LeandroVivaldini@users.noreply.github.com>
2024-02-22 20:38:14 -0300

    Aula 22-02

---
+++ repeticao.js
@@ -0,0 +1,30 @@
+let entrada = require('prompt-sync')();
+
+// let idade = entrada('Digite sua idade: ')
+
+// let idadeInt = parseInt(idade);
+
+// console.log('Digite sua idade: ');
+
+// if (idadeInt < 18) {
+//     console.log('Menor')
+// } else if (idadeInt == 18) {
+//     console.log('Tem 18')
+// }else {
+//     console.log('Maior')
+// }
+
+let num = parseInt(entrada('Digite um número para visualizar a tabuada!: '))
+
+// let cont = 1;
+
+// while (cont <=10) {
+//     let calc = num * cont;
+//     console.log(`${num} X ${cont} = ${calc}`);
+//     cont++;
+// }
+
+for (let cont=1; cont<=10; cont++) {
+    let calc = num*cont;
+    console.log(`${num} X ${cont} = ${calc}`)
+}
